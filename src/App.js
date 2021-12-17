aecbb5400ed7dc2959f390aeb9796785a3ff2080
Mekel Tensley <mekelintech@gmail.com>
2021-12-17 18:52:42 -0500

    Update props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Components 
+import Roster from './Roster';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Roster 
+                title="Women's Roster"
+                name="Sha'Carri Richardson"
+                type="100m Sprints"
+                height="5'9"
+                class="Junior"
+                />
+            </div>
+        )
+    }
 }
+
 
 export default App;
